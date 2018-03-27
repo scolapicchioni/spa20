26e9ad4f32a944d99f30abef0d6d09dd0a15eeae
Simona Colapicchioni <s.colapicchioni@gmail.com>
2018-03-27 08:58:20 +0200

    old labs deleted

--- Lab02_old/Solution/MarketPlace/MarketPlaceService/Models/Product.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MarketPlaceService.Models {
-    public class Product {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public decimal Price { get; set; }
-    }
-}
